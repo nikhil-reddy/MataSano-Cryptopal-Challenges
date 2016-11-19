d630244a3d9354ebc53ede627d030cabde366a74
Nikhil Reddy <nikhil.cvr@gmail.com>
2016-11-18 22:36:36 -0500

    Update challenge1-5.py

--- challenge1-5.py
+++ challenge1-5.py
@@ -1,15 +1,15 @@
 import binascii
-from Crypto.Util.strxor import strxor_c
+from Crypto.Util.strxor import strxor
 
 def xor_cipher(input,key):
         text = input
         key_hex = key
         length_text = len(text)
         length_key = len(key)
-        i = 0
+        # i = 0
         cipher= b''
         for i in range(length_text):
-                cipher+= str(strxor_c(text[i],key_hex[i%length_key]))
+                cipher+= (strxor(text[i],key_hex[i%length_key])).encode('hex')[1]
         return cipher
 
 input = raw_input("Enter String: ")
