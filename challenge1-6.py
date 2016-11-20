f9e979bd4180ce5fbba65ee7fd3d6b756686b9d5
Nikhil Reddy <nikhil.cvr@gmail.com>
2016-11-19 22:33:26 -0500

    Update challenge1-6.py
    edited code to find the keysize with the least "normalised" hamming distance.

--- challenge1-6.py
+++ challenge1-6.py
@@ -1,4 +1,5 @@
 import binascii
+import base64
 from Crypto.Util.strxor import strxor
 
 def edit_distance(str1, str2):
@@ -14,10 +15,38 @@ def edit_distance(str1, str2):
 
 	return hamming_distance
 
+def find_keysize(str):
+	min_dist = 9999999
+	final_keysize = 0
+	for i in xrange(2,40):
+		temp = calc_normalised_edit_distance(str , i)
+		if temp < min_dist:
+			min_dist = temp
+			final_keysize = i
+
+	return final_keysize
+
+
+def calc_normalised_edit_distance(str, keysize):
+
+	str_block1 = str[0:keysize]
+	str_block2 = str[keysize:keysize*2]
+
+	dist = edit_distance(str_block1, str_block2)
+	ans = dist/keysize
+
+	return ans
+
+
+
 
 
 str1 = "this is a test"
 str2 = "wokka wokka!!!"
-ans = edit_distance(str1,str2)
+f = open("file1-6.txt",'r')
+line = f.read()
+line_send= base64.b64decode(line)
+# print line_send
+ans = find_keysize(line_send)
 
 print ans
